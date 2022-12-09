d3cba3ef8cdf2e722c6afcaffab0ac70295eda38
Shuhui Xu <474497097@qq.com>
2022-12-09 16:09:25 +0800

    update

--- main.go
+++ main.go
@@ -1,23 +1,9 @@
 package main
 
 import (
-	"goal-layout/internal/router"
-	"goal-layout/pkg/core"
 
-	"os"
-	"os/signal"
-	"syscall"
 )
 
 func main() {
-
-	core.StartModule()
-
-	router.HttpServerRun()
-
-	quit := make(chan os.Signal)
-	signal.Notify(quit, syscall.SIGKILL, syscall.SIGQUIT, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
-	router.HttpServerStop()
 
 }
